b43cda4261d13711f6032e6cabea46fb08568a0b
Divesh Keswani <divkeswa@publicisgroupe.net>
2022-11-02 12:22:09 +0530

    TS issue fixed

--- pages/toggle/index.tsx
+++ pages/toggle/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // eslint-disable-next-line import/no-named-as-default
 import ToggleComponent from '../../utils/useToggle/ToggleComponent';
 
-export default function Toggle(props) {
+export default function Toggle() {
   return (
     <ToggleComponent />
   );
